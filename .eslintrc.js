ff0c1dc434927bba8fed1c31baed010948845bf3
Svyatoslav Ezhov <svyatoslav.ezhov@gmail.com>
2024-05-04 19:54:44 +0300

    prepare basic files

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:@typescript-eslint/recommended-requiring-type-checking",
     "plugin:prettier/recommended",
-    "prettier/@typescript-eslint",
+    "prettier",
     "plugin:import/errors",
     "plugin:import/warnings",
     "plugin:import/typescript"
