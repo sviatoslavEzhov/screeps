65518ba187e25b54b1b055deb39eca0de76b8cff
Svyatoslav Ezhov <svyatoslav.ezhov@gmail.com>
2024-05-05 15:00:00 +0300

    feat(prototype): add basic methods from tutorial
    * add prototypes for Creep prototype
    * add miner method
    * add builder method
    * add upgrader method
    * add work method

---
+++ src/proptotypes/Creep.ts
@@ -0,0 +1,82 @@
+Object.defineProperty(Creep.prototype, "hasFreeCapacity", {
+  get(this: Creep): boolean {
+    return this.store.getFreeCapacity() > 0;
+  }
+});
+
+Creep.prototype.miner = function (): void {
+  if (this.hasFreeCapacity) {
+    const sources = this.room.find(FIND_SOURCES);
+
+    if (this.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+      this.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
+    }
+  } else {
+    const targets = this.room.find(FIND_STRUCTURES, {
+      filter: structure => {
+        return (
+          (structure.structureType === STRUCTURE_EXTENSION || structure.structureType === STRUCTURE_SPAWN) &&
+          structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+        );
+      }
+    });
+
+    if (targets.length > 0) {
+      if (this.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+        this.moveTo(targets[0], { visualizePathStyle: { stroke: "#ffffff" } });
+      }
+    }
+  }
+};
+
+Creep.prototype.builder = function (): void {
+  if (this.memory.building && this.store[RESOURCE_ENERGY] === 0) {
+    this.memory.building = false;
+    this.say("🔄 harvest");
+  }
+  if (!this.memory.building && this.store.getFreeCapacity() === 0) {
+    this.memory.building = true;
+    this.say("🚧 build");
+  }
+
+  if (this.memory.building) {
+    const targets = this.room.find(FIND_CONSTRUCTION_SITES);
+    if (targets.length) {
+      if (this.build(targets[0]) === ERR_NOT_IN_RANGE) {
+        this.moveTo(targets[0], { visualizePathStyle: { stroke: "#ffffff" } });
+      }
+    }
+  } else {
+    const sources = this.room.find(FIND_SOURCES);
+    if (this.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+      this.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
+    }
+  }
+};
+
+Creep.prototype.upgrader = function (): void {
+  if (this.memory.upgrading && this.store[RESOURCE_ENERGY] === 0) {
+    this.memory.upgrading = false;
+    this.say("🔄 harvest");
+  }
+  if (!this.memory.upgrading && this.store.getFreeCapacity() === 0) {
+    this.memory.upgrading = true;
+    this.say("⚡ upgrade");
+  }
+
+  if (this.memory.upgrading && this.room.controller !== undefined) {
+    if (this.upgradeController(this.room.controller) === ERR_NOT_IN_RANGE) {
+      this.moveTo(this.room.controller, { visualizePathStyle: { stroke: "#ffffff" } });
+    }
+  } else {
+    const sources = this.room.find(FIND_SOURCES);
+    if (this.harvest(sources[0]) === ERR_NOT_IN_RANGE) {
+      this.moveTo(sources[0], { visualizePathStyle: { stroke: "#ffaa00" } });
+    }
+  }
+};
+
+Creep.prototype.work = function (): void {
+  const role = this.memory.role;
+  this[role]();
+};
